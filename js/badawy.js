941bc3dc8693bc3c59b8b0e6a3e7998e435bed2f
mahmoud-badawy <mhmwdbdwysyd5@gmail.com>
2026-06-18 00:17:59 +0300

    Add files via upload

--- js/badawy.js
+++ js/badawy.js
@@ -13,24 +13,3 @@ function revealSections() {
     }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    revealSections();
-});
-
-particlesJS("particles-js", {
-    "particles": {
-        "number": { "value": 50, "density": { "enable": true, "value_area": 700 } },
-        "color": { "value": "#a855f7" },
-        "shape": { "type": "circle" },
-        "opacity": { "value": 0.2, "random": true },
-        "size": { "value": 2.5, "random": true },
-        "line_linked": { "enable": true, "distance": 130, "color": "#3b82f6", "opacity": 0.08, "width": 1 },
-        "move": { "enable": true, "speed": 1.2, "direction": "none", "random": true, "straight": false, "out_mode": "out" }
-    },
-    "interactivity": {
-        "detect_on": "canvas",
-        "events": { "onhover": { "enable": true, "mode": "grab" } },
-        "modes": { "grab": { "distance": 130, "line_linked": { "opacity": 0.3 } } }
-    },
-    "retina_detect": true
-});
